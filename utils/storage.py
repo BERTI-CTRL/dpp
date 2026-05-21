88a0e1408f8cff6fc34834dd5765f54a74b2d2b6
Mateus Berti <mateusbertib@gmail.com>
2026-05-21 19:55:43 -0300

    atualizações storage.py funções

--- utils/storage.py
+++ utils/storage.py
@@ -2,7 +2,10 @@ import os
 import shutil
 from pathlib import Path
 from datetime import datetime
+from typing import final
 import pandas as pd
+
+
 '''
 def salvar_perfil(...)
 
@@ -13,25 +16,54 @@ def salvar_feedback(...)
 def carregar_conversas(...)
 
 def carregar_perfis(...)'''
+csv_perfis = Path(__file__).parents[1]/'dados'/'perfis.csv'
+csv_feedback = Path(__file__).parents[1]/'dados'/'feedback.csv'
+csv_conversas = Path(__file__).parents[1]/'dados'/'conversas.csv'
 
-def salvar_perfil(nome,idade,curso,hobbies,dificuldade,tema)
-    perfil_aluno = {'nome':nome,
+
+def salvar_perfil(nome,idade,curso,hobbies,dificuldade,tema):
+    path_csv_perfis = Path(__file__).parents[1]/'dados'/'perfis.csv'
+    path_csv_perfis.parent.mkdir(exist_ok=True)
+
+    perfil_aluno_novo =pd.DataFrame([{'nome':nome,
                     'idade':idade,
                     'curso':curso,
-                    'hobbies':hobbies,
+                    'hobbies':','.join(hobbies),
                     'dificuldade':dificuldade,
-                    'tema':tema}
+                    'tema':tema}])
 
-    return perfil_aluno
+
+    if path_csv_perfis.exists():   
+        antigo_perfis_csv = pd.read_csv(path_csv_perfis)
+        final = pd.concat([antigo_perfis_csv,perfil_aluno_novo],ignore_index=True)
+    else:
+        final = perfil_aluno_novo
+        
+    
+    final.to_csv(path_csv_perfis,index=False)
+
+    
 
 def salvar_conversa(session_id, nome,pergunta,resposta):
-    data = datetime.now()
-    celula_de_chat = pd.Dataframe({'nome':nome,
+    data_atual = datetime.now()
+    path_csv_conversas = Path(__file__).parents[1]/'dados'/'conversas.csv'
+    csv_conversas.parent.mkdir(exist_ok=True)
+
+
+    celula_de_chat = pd.DataFrame([{'nome':nome,
                                   'pergunta':pergunta,
                                   'resposta':resposta,
-                                  'session_id':session_id})
+                                  'session_id':session_id,
+                                  'data':data_atual}])
     
-    csv_conversas = pd.to
+    if path_csv_conversas.exists():
+        csv_conversas_antigo = pd.read_csv(path_csv_conversas)
+        final  = pd.concat([csv_conversas_antigo,celula_de_chat],ignore_index=True)
+
+    else:
+        final = celula_de_chat
+    
+    final.to_csv(path_csv_conversas,index=False)
 
 
 
@@ -47,4 +79,5 @@ def salvar_feedback(nota_reflexao,nota_contexto,comentarios,session_id):
 def carregar_conversas(celula_de_chat):
 
 
-def carregar_perfis(...):
+def carregar_perfis(perfis_csv):
+    perfis_carregados = pd.read_csv(perfis_csv)
